96723b8d68ec0e6ae29164db6e3c7bce22827e48
moisesjbc <moisesjbc@gmail.com>
2016-12-10 20:41:19 +0000

    Added spit sound

--- ldgc_37/Assets/Scripts/GobMovement.cs
+++ ldgc_37/Assets/Scripts/GobMovement.cs
@@ -8,6 +8,7 @@ public class GobMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		GetComponent<AudioSource>().Play (1);
 		score = GameObject.Find ("Score");
 	}
 	
--- ldgc_37/Assets/Scripts/Shooter.cs
+++ ldgc_37/Assets/Scripts/Shooter.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 	public GameObject gob;
 	public GameObject gobSpawnPoint;
-
 	public GameObject bottle;
 
 	// Use this for initialization
@@ -15,7 +14,7 @@ public class Shooter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Debug.Log (bottle.GetComponent <GapBarScript> ( ));
-		if (Input.GetMouseButtonDown (0) && bottle.GetComponent <GapBarScript> ( ).cur_gap > 0) {
+		if (Input.GetMouseButtonDown (0) && bottle.GetComponent <GapBarScript> ().cur_gap > 0) {
 			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			mousePosition.z = 1.0f;
 			Debug.Log ("mousePosition: " + mousePosition);
