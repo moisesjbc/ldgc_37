10abed032c054bb981173bbb40f485aba8abee25
moisesjbc <moisesjbc@gmail.com>
2016-12-10 18:02:53 +0000

    Added gob removal after 5 seconds.

--- ldgc_37/Assets/Scripts/GobMovement.cs
+++ ldgc_37/Assets/Scripts/GobMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GobMovement : MonoBehaviour {
 	public float speed = 5.0f;
+	float liveTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +11,11 @@ public class GobMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		liveTime += Time.deltaTime;
 		transform.Translate (transform.up * speed * Time.deltaTime);
+		if (liveTime > 5.0f) {
+			Destroy(gameObject);
+		}
 	}
 
 	void OnTriggerEnter2D (Collider2D col)
