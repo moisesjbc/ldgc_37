8be88162dea5496620a67c6bbfad0bc76fc59904
moisesjbc <moisesjbc@gmail.com>
2016-12-10 19:10:40 +0000

    Added timer.

---
+++ ldgc_37/Assets/Scripts/TimeUpdating.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class TimeUpdating : MonoBehaviour {
+	public float secondsRemaining = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		secondsRemaining -= Time.deltaTime;
+
+		int minutes = (int)(secondsRemaining / 60);
+		int seconds = (int)(secondsRemaining % 60);
+		GetComponent<Text> ().text = minutes.ToString () + ":" + seconds.ToString ("00");
+
+		if (minutes <= 0 && seconds <= 0) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
