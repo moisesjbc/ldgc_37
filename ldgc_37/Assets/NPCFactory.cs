12b76e800cb4c88ff769614100a8741c9cda56d6
moisesjbc <moisesjbc@gmail.com>
2016-12-10 16:02:06 +0000

    Added enemy spawning

---
+++ ldgc_37/Assets/NPCFactory.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCFactory : MonoBehaviour {
+	public float minY, maxY;
+	public float enemyCooldown = 1.0f;
+	public GameObject enemy;
+	float timeWithoutSpawningEnemy = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		SpawnEnemy ();
+	}
+
+	void SpawnEnemy(){
+		float x = -7.0f;
+		if (Random.Range (0, 100) < 50) {
+			x = 7.0f;
+		}
+		Vector3 respawnPoint = new Vector3(x, Random.Range (minY, maxY), 0.0f);
+		Instantiate (enemy, respawnPoint, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeWithoutSpawningEnemy += Time.deltaTime;
+		if(timeWithoutSpawningEnemy >= enemyCooldown){
+			SpawnEnemy();
+			timeWithoutSpawningEnemy = 0.0f;
+		}
+	}
+}
---
+++ ldgc_37/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	public float minSpeed = 5.0f;
+	public float maxSpeed = 10.0f;
+	float speed = 5.0f;
+	Vector3 movementDirection = Vector3.zero;
+	bool movingRight;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (minSpeed, maxSpeed);
+		if (transform.position.x < 0.0f) {
+			movingRight = true;
+			movementDirection = Vector3.right;
+		} else {
+			movingRight = false;
+			movementDirection = Vector3.left;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (movingRight && (transform.position.x > 10.0f)) {
+			Destroy (gameObject);
+		} else if (!movingRight && (transform.position.x < -10.0f)) {
+			Destroy (gameObject);
+		}
+		transform.Translate (movementDirection * speed * Time.deltaTime);
+	}
+}
