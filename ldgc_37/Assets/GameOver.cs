b14d3983c5e7eaceb1313add246de194ae1286ef
moisesjbc <moisesjbc@gmail.com>
2016-12-11 12:01:32 +0000

    Added gameover

---
+++ ldgc_37/Assets/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<Image> ().enabled == true && Input.anyKeyDown) {
+			GetComponent<Image> ().enabled = false;
+			Application.LoadLevel (Application.loadedLevel);
+			Time.timeScale = 1.0f;
+		}
+	}
+}
