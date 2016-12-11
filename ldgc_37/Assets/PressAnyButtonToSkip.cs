2bf294296344e3ec86416d0c58b0ba93e22d34cd
moisesjbc <moisesjbc@gmail.com>
2016-12-11 13:10:40 +0000

    Added intro animation

---
+++ ldgc_37/Assets/PressAnyButtonToSkip.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressAnyButtonToSkip : MonoBehaviour {
+	public string targetSceneName = "MainScene";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			Application.LoadLevel (targetSceneName);
+		}
+	}
+}
